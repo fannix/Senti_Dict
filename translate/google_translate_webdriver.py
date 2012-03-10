732c47d55c6e8913a301a4275571da031ae0e977
Xinfan Meng <mxf3306@gmail.com>
2012-03-10 15:39:21 +0800

    Direct post lexicon content to google translate. The size of content seems too large to be post.

--- translate/google_translate_webdriver.py
+++ translate/google_translate_webdriver.py
@@ -1,11 +1,30 @@
+"""
+Translate sentiment lexicon using selenium
+"""
 from selenium import webdriver
+import sys
 
-driver = webdriver.Firefox()
-driver.implicitly_wait(30)
-base_url = "http://translate.google.cn/"
-driver.get(base_url + "/?hl=en#en|zh-CN|")
-driver.find_element_by_id("source").clear()
-driver.find_element_by_id("source").send_keys("hello\nworld\ngood bye")
-driver.find_element_by_id("gt-submit").click()
-print driver.find_element_by_id("result_box").text
-driver.quit()
+def translate(words):
+    """
+    Translate English words into Chinese
+
+    Parameters
+    ----------
+    words: English words
+    """
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(30)
+    base_url = "http://translate.google.cn/"
+    driver.get(base_url + "/?hl=en#en|zh-CN|")
+    driver.find_element_by_id("source").clear()
+    driver.find_element_by_id("source").send_keys("".join(words))
+    driver.find_element_by_id("gt-submit").click()
+    print driver.find_element_by_id("result_box").text
+    driver.quit()
+
+if __name__ == "__main__":
+    lexicon_file = sys.argv[1]
+    #words = open(lexicon_file).readlines()[:100]
+    words = open(lexicon_file).readlines()
+    print words
+    translate(words)
