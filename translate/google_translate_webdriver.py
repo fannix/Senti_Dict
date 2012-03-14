70519f5bb06d3e75af387a84daed65d274a05025
Xinfan Meng <mxf3306@gmail.com>
2012-03-14 09:50:52 +0800

    For efficiency, avoid sending data directly, instead exchange data by using system clipboard

--- translate/google_translate_webdriver.py
+++ translate/google_translate_webdriver.py
@@ -2,29 +2,25 @@
 Translate sentiment lexicon using selenium
 """
 from selenium import webdriver
+from selenium.webdriver.common import keys
 import sys
 
-def translate(words):
-    """
-    Translate English words into Chinese
 
-    Parameters
-    ----------
-    words: English words
+def translate():
+    """
+    Obtain English from the clipboard and translate them into Chinese
     """
     driver = webdriver.Firefox()
-    driver.implicitly_wait(30)
     base_url = "http://translate.google.cn/"
     driver.get(base_url + "/?hl=en#en|zh-CN|")
     driver.find_element_by_id("source").clear()
-    driver.find_element_by_id("source").send_keys("".join(words))
-    driver.find_element_by_id("gt-submit").click()
-    print driver.find_element_by_id("result_box").text
+    #driver.find_element_by_id("source").send_keys("".join(words)) #don't do that, too slow
+    driver.find_element_by_id("source").send_keys(keys.Keys.COMMAND + "v")
+    #driver.find_element_by_id("gt-submit").click()
+    while len(driver.find_element_by_id("result_box").text) < 3:
+        pass
+    print driver.find_element_by_id("result_box").text.encode("utf-8")
     driver.quit()
 
 if __name__ == "__main__":
-    lexicon_file = sys.argv[1]
-    #words = open(lexicon_file).readlines()[:100]
-    words = open(lexicon_file).readlines()
-    print words
-    translate(words)
+    translate()
